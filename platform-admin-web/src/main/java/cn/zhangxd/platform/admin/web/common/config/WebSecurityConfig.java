9252cce0e8dc092128ac3c3615c2df143e7ff63f
sWX239227 <songdi1@huawei.com>
2017-05-24 14:07:20 +0800

    first commit

---
+++ platform-admin-web/src/main/java/cn/zhangxd/platform/admin/web/common/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package cn.zhangxd.platform.admin.web.common.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import cn.zhangxd.platform.common.web.config.AbstractWebSecurityConfig;
+
+/**
+ * spring-security配置
+ *
+ * @author zhangxd
+ */
+@Configuration
+public class WebSecurityConfig extends AbstractWebSecurityConfig {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/auth/token").permitAll()
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll();//就是这一行啦  ;
+        super.configure(security);
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://10.61.16.103:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));//Allow: GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://10.61.16.103:8080");
+            }
+        };
+    }
+}
