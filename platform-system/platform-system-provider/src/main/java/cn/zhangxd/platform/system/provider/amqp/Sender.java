a3820ab39e121fbdf9893fe5c8791f964dd26b25
sWX239227 <songdi1@huawei.com>
2017-05-25 10:43:31 +0800

    添加日志打印

--- platform-system/platform-system-provider/src/main/java/cn/zhangxd/platform/system/provider/amqp/Sender.java
+++ platform-system/platform-system-provider/src/main/java/cn/zhangxd/platform/system/provider/amqp/Sender.java
@@ -1,17 +1,25 @@
+
 package cn.zhangxd.platform.system.provider.amqp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-public class Sender {
-
-	@Autowired
+@Component
+public class Sender
+{
+    private static Logger logger = LoggerFactory.getLogger( Sender.class );
+    @Autowired
     private RabbitTemplate rabbitTemplate;
 
-	@Scheduled(fixedDelay = 10000L)
-	public void send() {
-		this.rabbitTemplate.convertAndSend("foo", "hello");
-	}
+    @Scheduled( fixedDelay = 10000L )
+    public void send()
+    {
+        logger.info( "发送消息---" );
+        this.rabbitTemplate.convertAndSend( "foo", "hello" );
+    }
 
 }
